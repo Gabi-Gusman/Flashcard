57202a9e419df6ac5e903d6aaa38372307518a6e
Gabi-Gusman <gabriellegusman12@gmail.com>
2024-11-19 13:36:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Os sete maridos de Evelyn Hugo',
+    ' Por que Evelyn escolhe Monique para escrever sua biografia?',
+    'Evelyn escolhe Monique porque há uma conexão específica entre o passado dela e o de Monique, um segredo que será revelado ao longo da narrativa e que impacta profundamente a jornalista.'
+)
+
+criaCartao(
+    'Jantar Secreto',
+    'Qual é o propósito do "jantar secreto" organizado por um grupo de amigos?',
+    ' O jantar secreto, criado para arrecadar dinheiro, envolve refeições clandestinas com carne humana, levando os amigos a situações perigosas e desafiando seus limites morais.'
+)
+
+criaCartao(
+    'Dias perfeitos',
+    'Qual é a motivação de Teo para sequestrar Clarice em Dias Perfeitos?',
+    'Teo, um estudante de medicina obcecado por Clarice, a sequestra na esperança de conquistá-la e fazê-la se apaixonar, apesar de sua resistência e medo.'
+)
+
+criaCartao(
+    'O Vilarejo',
+    'Qual é a ligação entre os contos presentes no livro O Vilarejo?',
+    'Os contos de O Vilarejo estão interligados por sete pecados capitais e se passam em uma vila isolada, onde cada história revela um lado sombrio dos moradores, mostrando o quanto o ser humano pode ser cruel e violento.'
+)
